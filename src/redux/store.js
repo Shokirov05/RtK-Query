bd8c3748704fff4f1f1d83b387f7a265fcc033f1
Shokirov05 <muhammaddiyorshokirov92@gmail.com>
2024-12-19 15:58:45 +0500

    Birinchi commit

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { todosApiSlice } from "./todosApiSlice";
+
+export const store = configureStore({
+    reducer: {
+        [todosApiSlice.reducerPath]: todosApiSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware)=>
+        getDefaultMiddleware().concat(todosApiSlice.middleware),
+});
